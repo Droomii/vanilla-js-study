8e49645518826b0f586257a000df03336ef2d8e7
Droomii <dowoo2594@naver.com>
2022-11-02 17:36:55 +0900

    대공사

--- src/App.ts
+++ src/App.ts
@@ -4,18 +4,20 @@ import Watcher from './define/Watcher';
 function App() {
     const {activeMenu, numbers} = Store;
 
-
-
     const buttons = numbers.map(num => {
         const onClick = () => {
             num.set(v => v + 1);
         };
 
+        const hi = Watcher('div', {}, (el) => {
+            el.replaceChildren(num.value < 3 ? 'hi!!' : '');
+        }, 'hi');
+
         return Watcher('button', {num}, (el, {num}) => {
-            el.innerHTML = `${num}번 클릭함`;
+            const children = [`${num}번 클릭함`, num.value < 5 ? hi : ''].filter(v => v);
+            el.replaceChildren(...children);
             el.style.display = 'block';
             el.addEventListener('click', onClick);
-            console.log('render');
         });
     });
 
@@ -24,7 +26,7 @@ function App() {
             `현재 path는 ${activeMenu}입니다.`,
             ...buttons
         );
-    });
+    }, 'root');
 }
 
 export default App;
--- src/define/Watcher.ts
+++ src/define/Watcher.ts
@@ -2,14 +2,24 @@ import State from './State';
 
 export interface RenderFunction {
     (): void;
-    el: HTMLElement;
+    el: HTMLElement & {cleanup: () => void;};
 }
 
-function Watcher<States extends Record<string, State<unknown>>, ElementType extends keyof HTMLElementTagNameMap>(
+interface Watchlist {
+    [key: string]: State<unknown>
+}
+
+function Watcher<States extends Watchlist, ElementType extends keyof HTMLElementTagNameMap>(
     element: ElementType,
     watchlist: States,
-    render: (el: HTMLElementTagNameMap[ElementType], states: States) => void): Node {
-    const el = document.createElement(element);
+    render: (el: HTMLElementTagNameMap[ElementType], states: States) => void, debug?: string): Node {
+    const el = document.createElement(element) as HTMLElementTagNameMap[ElementType] & {cleanup: () => void;};
+    el.cleanup = () => {
+        console.log('cleanup', el);
+        Object.values(watchlist).forEach(v => v.removeListener(renderFunc));
+        observer.disconnect();
+    };
+
     let isRenderPrepared = false;
 
     const renderFunc: RenderFunction = () => {
@@ -26,8 +36,18 @@ function Watcher<States extends Record<string, State<unknown>>, ElementType exte
     };
 
     renderFunc.el = el;
-
     Object.values(watchlist).forEach(v => v.addListener(renderFunc));
+    const observer = new MutationObserver((entries) => {
+        debug && console.log('debug', debug);
+        entries.forEach(v => {
+            v.removedNodes.forEach(v => {
+                if (v.isConnected) return;
+                (v as unknown as {cleanup: () => void}).cleanup?.();
+            });
+        });
+    });
+
+    observer.observe(el, {childList: true, subtree: true});
 
     renderFunc();
     return el;
